914c2ae8e3b881f62ca987c1c3343df6a034d865
Ingy döt Net <ingy@ingy.net>
2020-11-11 14:47:21 -0500

    git subrepo pull ext/bashplus
    subrepo:
      subdir:   "ext/bashplus"
      merged:   "e49f45a"
    upstream:
      origin:   "git@github.com:ingydotnet/bashplus.git"
      branch:   "master"
      commit:   "e49f45a"
    git-subrepo:
      version:  "0.4.1"
      origin:   "git@github.com:ingydotnet/git-subrepo"
      commit:   "a04d8c2"

--- ext/bashplus/lib/bash+.bash
+++ ext/bashplus/lib/bash+.bash
@@ -2,33 +2,65 @@
 #
 # Copyright (c) 2013-2020 Ingy döt Net
 
-{
-  bash+:version-check() {
-    test $1 -ge 4 && return
-    test $1 -eq 3 -a $2 -ge 2 && return
-    echo "Bash version 3.2 or higher required for 'git hub'" >&2
-    exit 1
-  }
-  bash+:version-check "${BASH_VERSINFO[@]}"
-  unset -f Bash:version-check
-}
-
 set -e
 
-[ -z "$BASHPLUS_VERSION" ] || return 0
+[[ ${BASHPLUS_VERSION-} ]] && return 0
 
-BASHPLUS_VERSION='0.0.8'
+BASHPLUS_VERSION=0.0.9
 
-@() { echo "$@"; }
-bash+:export:std() { @ use die warn; }
+bash+:version-check() {
+  local cmd want got out
+
+  IFS=' ' read -r -a cmd <<< "${1:?}"
+  IFS=. read -r -a want <<< "${2:?}"
+  : "${want[2]:=0}"
+
+  if [[ ${cmd[*]} == bash ]]; then
+    got=("${BASH_VERSINFO[@]}")
+    BASHPLUS_VERSION_CHECK=${BASH_VERSION-}
+  else
+    [[ ${#cmd[*]} -gt 1 ]] || cmd+=(--version)
+    out=$("${cmd[@]}") ||
+      { echo "Failed to run '${cmd[*]}'" >&2; exit 1; }
+    [[ $out =~ ([0-9]+\.[0-9]+(\.[0-9]+)?) ]] ||
+      { echo "Can't determine version number from '${cmd[*]}'" >&2; exit 1; }
+    BASHPLUS_VERSION_CHECK=${BASH_REMATCH[1]}
+    IFS=. read -r -a got <<< "$BASHPLUS_VERSION_CHECK"
+  fi
+  : "${got[2]:=0}"
+
+  ((
+    got[0] > want[0] ||
+    got[0] == want[0] && got[1] > want[1] ||
+    got[0] == want[0] && got[1] == want[1] && got[2] >= want[2]
+  )) || return 1
+
+  return 0
+}
+
+bash+:version-check bash 3.2 ||
+  { echo "The 'bashplus' library requires 'Bash 3.2+'." >&2; exit 1; }
+
+@() (echo "$@")  # XXX do we want to keep this?
+
+bash+:export:std() {
+  set -o pipefail
+
+  if bash+:version-check bash 4.4; then
+    set -o nounset
+    shopt -s inherit_errexit
+  fi
+
+  echo use die warn
+}
 
 # Source a bash library call import on it:
 bash+:use() {
-  local library_name="${1:?bash+:use requires library name}"; shift
-  local library_path=; library_path="$(bash+:findlib $library_name)"
-  [[ -n $library_path ]] || {
+  local library_name=${1:?bash+:use requires library name}; shift
+  local library_path=; library_path=$(bash+:findlib "$library_name") || true
+  [[ $library_path ]] ||
     bash+:die "Can't find library '$library_name'." 1
-  }
+
   source "$library_path"
   if bash+:can "$library_name:import"; then
     "$library_name:import" "$@"
@@ -42,9 +74,11 @@ bash+:import() {
   local arg=
   for arg; do
     if [[ $arg =~ ^: ]]; then
-      bash+:import `bash+:export$arg`
+      # Word splitting required here
+      # shellcheck disable=2046
+      bash+:import $(bash+:export"$arg")
     else
-      bash+:fcopy bash+:$arg $arg
+      bash+:fcopy "bash+:$arg" "$arg"
     fi
   done
 }
@@ -53,38 +87,51 @@ bash+:import() {
 bash+:fcopy() {
   bash+:can "${1:?bash+:fcopy requires an input function name}" ||
     bash+:die "'$1' is not a function" 2
-  local func=; func=$(type "$1" 3>/dev/null | tail -n+3)
-  [[ -n $3 ]] && "$3"
+  local func
+  func=$(type "$1" 3>/dev/null | tail -n+3)
+  [[ ${3-} ]] && "$3"
   eval "${2:?bash+:fcopy requires an output function name}() $func"
 }
 
 # Find the path of a library
 bash+:findlib() {
-  local library_name=; library_name="$(tr 'A-Z' 'a-z' <<< "${1//:://}").bash"
-  local lib="${BASHPLUSLIB:-${BASHLIB:-$PATH}}"
-  library_name="${library_name//+/\\+}"
-  ( IFS=':'; find $lib -name ${library_name##*/} 2>/dev/null |
+  local library_name
+  library_name=$(tr '[:upper:]' '[:lower:]' <<< "${1//:://}").bash
+  local lib=${BASHPLUSLIB:-${BASHLIB:-$PATH}}
+  library_name=${library_name//+/\\+}
+  IFS=':' read -r -a libs <<< "$lib"
+  find "${libs[@]}" -name "${library_name##*/}" 2>/dev/null |
     grep -E "$library_name\$" |
-    head -n1 )
+    head -n1
 }
 
 bash+:die() {
-  local msg="${1:-Died}"
-  printf "${msg//\\n/$'\n'}" >&2
-  local trailing_newline_re=$'\n''$'
-  [[ $msg =~ $trailing_newline_re ]] && exit 1
+  local msg=${1:-Died}
+  msg=${msg//\\n/$'\n'}
 
-  local c=($(caller ${DIE_STACK_LEVEL:-${2:-0}}))
-  (( ${#c[@]} == 2 )) &&
-    msg=" at line %d of %s" ||
+  printf "%s" "$msg" >&2
+  if [[ $msg == *$'\n' ]]; then
+    exit 1
+  else
+    printf "\n"
+  fi
+
+  local c
+  IFS=' ' read -r -a c <<< "$(caller "${DIE_STACK_LEVEL:-${2:-0}}")"
+  if (( ${#c[@]} == 2 )); then
+    msg=" at line %d of %s"
+  else
     msg=" at line %d in %s of %s"
-  printf "$msg\n" ${c[@]} >&2
+  fi
+
+  # shellcheck disable=2059
+  printf "$msg\n" "${c[@]}" >&2
   exit 1
 }
 
 bash+:warn() {
-  local msg="${1:-Warning}"
-  printf "${msg//\\n/$'\n'}\n" >&2
+  local msg=${1:-Warning}
+  printf "%s" "${msg//\\n/$'\n'}\n" >&2
 }
 
 bash+:can() {
--- ext/bashplus/test/source-bash+-std.t
+++ ext/bashplus/test/source-bash+-std.t
@@ -1,18 +1,18 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
 
-source test/test.bash
+source test/setup
 
 PATH=$PWD/bin:$PATH
 source bash+ :std
 
-ok "`bash+:can use`"         'use is imported'
-ok "`bash+:can die`"         'die is imported'
-ok "`bash+:can warn`"        'warn is imported'
+ok "$(bash+:can use)"         'use is imported'
+ok "$(bash+:can die)"         'die is imported'
+ok "$(bash+:can warn)"        'warn is imported'
 
-ok "`! bash+:can import`"    'import is not imported'
-ok "`! bash+:can main`"      'main is not imported'
-ok "`! bash+:can fcopy`"     'fcopy is not imported'
-ok "`! bash+:can findlib`"   'findlib is not imported'
-ok "`! bash+:can can`"       'can is not imported'
+ok "$(! bash+:can import)"    'import is not imported'
+ok "$(! bash+:can main)"      'main is not imported'
+ok "$(! bash+:can fcopy)"     'fcopy is not imported'
+ok "$(! bash+:can findlib)"   'findlib is not imported'
+ok "$(! bash+:can can)"       'can is not imported'
 
 done_testing 8
